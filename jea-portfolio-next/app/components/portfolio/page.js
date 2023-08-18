1a4d3de3c5e6a30e5a359ed87f7d99c582958c07
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-08-18 16:09:40 +0000

    Changes to be committed: 	modified:   jea-portfolio-next/app/components/portfolio/page.js

--- jea-portfolio-next/app/components/portfolio/page.js
+++ jea-portfolio-next/app/components/portfolio/page.js
@@ -17,47 +17,47 @@ export default function Work(){
 
             <div id="portfolio" className="portfolio">
                 <div id="btnFilter">
-                    <button className="btn active" onclick="filterSelection('all')"> Tout</button>
-                    <button className="btn" onclick="filterSelection('pictures')">
+                    <button className="btn active" > Tout</button>
+                    <button className="btn">
                         Photographies</button>
-                    <button className="btn" onclick="filterSelection('website')">Sites web</button>
+                    <button className="btn" >Sites web</button>
                 </div>
 
                 <div className="container">
                     <div className="filter pictures">
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt />
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img" />
                         <div className="link-portfolio">
                             <a href="assets/images/skooter Marrakech.jpg" target="_blank">Quad Marrakech</a>
                         </div>
                     </div>
                     <div className="filter pictures">
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt />
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img" />
                         <div className="link-portfolio">
                             <a href="assets/images/JEALIFE_Pictures.jpg" target="_blank">JEALIFE Pictures</a>
                         </div>
                     </div>
                     <div className="filter website">
 
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt/>
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img"/>
                         <div className="link-portfolio">
                             <a href="https://tirem-t.vercel.app/" target="_blank"> TiremT</a>
                         </div>
                     </div>
 
                     <div className="filter website">
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt/>
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img"/>
                         <div className="link-portfolio">
                             <a href="https://jea-portfolio.vercel.app/" target="_blank"> JEaLiFe</a>
                         </div>
                     </div>
                     <div className="filter pictures">
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt/>
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img"/>
                         <div className="link-portfolio">
                             <a href="assets/images/girl.jpg" target="_blank">Girl</a>
                         </div>
                     </div>
                     <div className="filter website">
-                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt />
+                        <img src="https://jean-portfolio-six.vercel.app/assets/images/skooter%20Marrakech.jpg" alt="img" />
                         <div className="link-portfolio">
                             <a href="https://flex-fit-tau.vercel.app/" target="_blank"> FlexFit</a>
                         </div>
